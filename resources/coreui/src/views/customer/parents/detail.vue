7977c2d1d8abd3a60311c3febb3b0143199e7137
thangnm123 <thanhcong1710@gmail.com>
2021-08-19 10:59:16 +0700

    update

--- resources/coreui/src/views/customer/parents/detail.vue
+++ resources/coreui/src/views/customer/parents/detail.vue
@@ -60,46 +60,24 @@
                   <p>Người tạo: <span class="fl-right">{{parent.source_name}}</span></p>
                 </div>
                 <div class="col-sm-9">
-                  <ul class="nav nav-tabs" id="myTab1" role="tablist">
-<li class="nav-item"><a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Chăm sóc</a></li>
- <li class="nav-item"><a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Học sinh</a></li>
-<li class="nav-item"><a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Lịch sử cập nhật</a></li>
-</ul>
-<div class="tab-content" id="myTab1Content">
-<div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro
-synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher
-retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip
-placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.
-</div>
-<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1
-labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft
-beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad
-vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica
-VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson
-8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester
-stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.
-</div>
-<div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro
-fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone
-skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings
-gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel
-fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer
-blog stumptown. Pitchfork sustainable tofu synth chambray yr.
-</div>
-</div>
+                  <ul class="nav nav-tabs nav-justified">
+                    <li class="nav-item">
+                      <a class="nav-link" @click.prevent="setActive('customer_care')" :class="{ active: isActive('customer_care') }" href="#customer_care">Chăm sóc</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" @click.prevent="setActive('students')" :class="{ active: isActive('students') }" href="#students">Học sinh</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" @click.prevent="setActive('logs')" :class="{ active: isActive('logs') }" href="#logs">Lịch sử cập nhật</a>
+                    </li>
+                  </ul>
+                  <div class="tab-content py-3" id="myTabContent">
+                    <div class="tab-pane fade" :class="{ 'active show': isActive('customer_care') }" id="customer_care">Home content</div>
+                    <div class="tab-pane fade" :class="{ 'active show': isActive('students') }" id="students">Profile content</div>
+                    <div class="tab-pane fade" :class="{ 'active show': isActive('logs') }" id="logs">Contact content</div>
+                  </div>
                 </div>
               </div>
-          </div>
-          <div class="card-footer">
-            <router-link class="btn btn-danger" :to="`/parents`">
-              <i class="fas fa-undo-alt"></i> Hủy
-            </router-link>
-            <button class="btn btn-success" type="button" @click="save">
-              <i class="fas fa-save"></i> Lưu
-            </button>
           </div>
         </div>
       </div>
@@ -211,22 +189,10 @@ export default {
         district:"",
         address:""
       },
-      tmp_district_id:"",
+      activeItem: 'customer_care'
     };
   },
   created() {
-    u.g(`/api/provinces`)
-      .then(response => {
-      this.html.province.list = response.data
-    })
-    u.g(`/api/jobs`)
-      .then(response => {
-      this.html.jobs.list = response.data
-    })
-    u.g(`/api/sources`)
-      .then(response => {
-      this.html.source.list = response.data
-    })
     this.loading.processing = true;
     u.g(`/api/parents/detail/${this.$route.params.id}`)
       .then(response => {
@@ -239,52 +205,11 @@ export default {
     })
   },
   methods: {
-    selectDate(date) {
-      if (date) {
-        this.student.birthday = moment(date).format("YYYY-MM-DD");
-      }
+    isActive (menuItem) {
+      return this.activeItem === menuItem
     },
-    save() {
-      let mess = "";
-      let resp = true;
-      if (this.parent.gender == "") {
-        mess += " - Danh xưng không được để trống<br/>";
-        resp = false;
-      }
-      if (this.parent.name == "") {
-        mess += " - Họ tên không được để trống<br/>";
-        resp = false;
-      }
-      if (this.parent.mobile_1 == "") {
-        mess += " - Số điện thoại không được để trống<br/>";
-        resp = false;
-      }
-      if (this.parent.mobile_1 != "" && !u.vld.phone(this.parent.mobile_1)) {
-        mess += " - Số điện thoại không đúng định dạng<br/>";
-        resp = false;
-      }
-      if (!resp) {
-        this.modal.color = "warning";
-        this.modal.body = mess;
-        this.modal.show = true;
-        this.modal.action_exit = "close";
-        return false;
-      }
-      this.parent.note = tinymce.get("input_tinymce").getContent();
-      this.loading.processing = true;
-      u.p(`/api/parents/update/${this.$route.params.id}`,this.parent)
-        .then((response) => {
-          this.loading.processing = false;
-          if (response.status == 200) {
-            this.modal.color = "success";
-            this.modal.body = "Cập nhật khách hàng thành công";
-            this.modal.show = true;
-            this.modal.action_exit = "exit";
-          }
-        })
-        .catch((e) => {
-          u.processAuthen(e);
-        });
+    setActive (menuItem) {
+      this.activeItem = menuItem
     },
     exit() {
       if (this.modal.action_exit == "exit") {
@@ -293,60 +218,6 @@ export default {
         this.modal.show = false;
       }
     },
-    getDistrict(data = null){
-      if (data && typeof data === 'object') {
-        const province_id = data.id
-        this.parent.province = data
-        this.parent.province_id = province_id
-        this.loading.processing = true
-        u.g(`/api/provinces/${province_id}/districts`).then(response => {
-          this.loading.processing = false
-          this.html.district.list = response.data
-          if(this.tmp_district_id){
-            this.parent.district = this.html.district.list.filter(item => item.id == this.tmp_district_id)[0]
-          }else{
-            this.parent.district_id = ""
-            this.parent.district = ""
-          } 
-        })
-      }else{
-        this.parent.province = ""
-        this.parent.province_id = ""
-        this.html.district.list = []
-        this.parent.district = ""
-        this.parent.district_id = ""
-      }
-    },
-    saveDistrict(data = null){
-      if (data && typeof data === 'object') {
-        const district_id = data.id
-        this.parent.district = data
-        this.parent.district_id = district_id
-      }else{
-        this.parent.district = ""
-        this.parent.district_id = ""
-      }
-    },
-    saveJob(data = null){
-      if (data && typeof data === 'object') {
-        const job_id = data.id
-        this.parent.job = data
-        this.parent.job_id = job_id
-      }else{
-        this.parent.job = ""
-        this.parent.job_id = ""
-      }
-    },
-    saveSource(data = null){
-      if (data && typeof data === 'object') {
-        const source_id = data.id
-        this.parent.source = data
-        this.parent.source_id = source_id
-      }else{
-        this.parent.source = ""
-        this.parent.source_id = ""
-      }
-    }
   },
 };
 </script>
