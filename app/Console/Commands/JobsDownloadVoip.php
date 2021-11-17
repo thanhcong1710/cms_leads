23e7423f401004ef801e7fa314022016a853306e
conglt <thanhcong1710@gmail.com>
2021-11-17 22:24:46 +0700

    update

--- app/Console/Commands/JobsDownloadVoip.php
+++ app/Console/Commands/JobsDownloadVoip.php
@@ -41,7 +41,7 @@ class JobsDownloadVoip extends Command
      */
     public function handle(Request $request)
     {
-        $last_time = date('Y-m-d h:i:s',time()-3600);
+        $last_time = date('Y-m-d H:i:s',time()-3600);
         $list_call = u::query("SELECT * FROM voip24h_data WHERE process_data = 0 AND created_at>'$last_time' ORDER BY id DESC");
         foreach($list_call AS $row){
             $voipControll = new VoipController();
