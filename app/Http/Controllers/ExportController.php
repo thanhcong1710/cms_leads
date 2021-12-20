1c0719498d7ab177f0d117e6ccea5db7d63780a2
conglt <thanhcong1710@gmail.com>
2021-12-20 23:19:04 +0700

    update

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -82,6 +82,7 @@ class ExportController extends Controller
     }
     public function report01(Request $request , $key,$value) {
         set_time_limit(300);
+        ini_set('memory_limit', '512M');
         $cond = " 1 ";
         $arr_key =explode(',',$key);
         $arr_value =explode(',',$value);
