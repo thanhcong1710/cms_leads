f90889513d1cc3bb65b8cb942aaad0a62e9615ae
conglt <thanhcong1710@gmail.com>
2022-08-10 17:01:36 +0700

    update

--- app/Http/Controllers/ParentsController.php
+++ app/Http/Controllers/ParentsController.php
@@ -38,7 +38,7 @@ class ParentsController extends Controller
             $cond .= " AND p.owner_id IN (".$request->user_info->users_manager.")";
         }
         if($request->user()->hasRole('Marketing')){
-            $cond .= " AND (p.creator_id IN (".$request->user()->id.") OR p.owner_id = ".$request->user()->id.")";
+            $cond .= " AND (p.creator_id IN (".$request->user()->id.") OR p.owner_id = ".$request->user()->id." OR p.source_id=26)";
         }
         if (!empty($status)) {
             $cond .= " AND p.status IN (".implode(",",$status).")";
