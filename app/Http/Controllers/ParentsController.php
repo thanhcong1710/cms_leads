1c39d7984b9594e34745090d5bb3bb7a385246f9
conglt <thanhcong1710@gmail.com>
2023-02-17 10:55:57 +0700

    update

--- app/Http/Controllers/ParentsController.php
+++ app/Http/Controllers/ParentsController.php
@@ -35,14 +35,14 @@ class ParentsController extends Controller
         $limitation =  $limit > 0 ? " LIMIT $offset, $limit": "";
         $cond = " 1 ";
         if(!$request->user()->hasRole('admin') && !$request->user()->hasRole('Supervisor') && !$request->user()->hasRole('Marketing')){
+            $cond .= " AND p.owner_id IN (".$request->user_info->users_manager.")";
+        }
+        if($request->user()->hasRole('Marketing')){
             if($request->user()->id== 21){
                 $cond .= " AND ((p.owner_id IN (".$request->user_info->users_manager.") AND p.owner_id NOT IN (".$request->user_info->tmp_users_manager.")) OR p.source_id=27 OR p.source_id=35)";
             }else{
-                $cond .= " AND p.owner_id IN (".$request->user_info->users_manager.")";
+                $cond .= " AND (p.creator_id IN (".$request->user()->id.") OR p.owner_id = ".$request->user()->id." OR p.source_id=26)";
             }
-        }
-        if($request->user()->hasRole('Marketing')){
-            $cond .= " AND (p.creator_id IN (".$request->user()->id.") OR p.owner_id = ".$request->user()->id." OR p.source_id=26)";
         }
         if (!empty($status)) {
             $cond .= " AND p.status IN (".implode(",",$status).")";
@@ -212,7 +212,7 @@ class ParentsController extends Controller
         }elseif(!$request->user()->hasRole('admin') && !$request->user()->hasRole('Supervisor') && !$request->user()->hasRole('Marketing')){
             $cond .= " AND p.owner_id IN (".$request->user_info->users_manager.")";
         }
-        
+        dd($cond);
         $data = u::first("SELECT p.*,(SELECT name FROM users WHERE id=p.creator_id) AS creator_name,
                 (SELECT name FROM cms_districts WHERE id=p.district_id) AS district_name,
                 (SELECT name FROM cms_provinces WHERE id=p.province_id) AS province_name,
