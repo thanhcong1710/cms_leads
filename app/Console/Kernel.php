004aabfe833934f927b44711c1d58642f1a134fc
conglt <thanhcong1710@gmail.com>
2023-12-31 00:09:27 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends ConsoleKernel
         '\App\Console\Commands\JobsProcessReport',
         '\App\Console\Commands\JobsProcessImportCheckin',
         '\App\Console\Commands\JobsProcessLockParent',
+        '\App\Console\Commands\JobsProcessDataImport',
     ];
 
     /**
@@ -27,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('jobsDownloadVoip:command')->cron('*/15 * * * *');
+        // $schedule->command('jobsDownloadVoip:command')->cron('*/15 * * * *');
         $schedule->command('jobsProcessReport:command')->everySixHours();
         // $schedule->command('jobsProcessImportCheckin:command')->cron('*/3 * * * *');
         $schedule->command('jobsProcessLockParent:command')->cron('0 0 * * *');
