279ef77cb52f67730b22f7d9020b1d3ce0ee9222
conglt <thanhcong1710@gmail.com>
2024-09-27 10:14:25 +0700

    update

--- resources/views/coreui/homepage.blade.php
+++ resources/views/coreui/homepage.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="locale" content="{{ App::getLocale() }}"/>
-    <link rel="icon" href="<%= BASE_URL %>favicon.ico">
+    <link rel="icon" href="favicon.png">
     <title>CMS Edu Việt Nam</title>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
