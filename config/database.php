f734826f84a1e7617b260081977be5062dc0f0b5
Cong Luong <conglt@9pay.vn>
2024-11-03 20:17:35 +0700

    update

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' =>  'cms_crm',
+            'database' =>  'logiclab_cms_crm',
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
