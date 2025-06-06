8b1600028d476e14cb0d241ce6e16962832b9829
thanhcong1710 <thanhcong1710@gmail.com>
2025-06-06 11:56:06 +0700

    update

--- app/Http/Controllers/VoipController.php
+++ app/Http/Controllers/VoipController.php
@@ -21,35 +21,109 @@ class VoipController extends Controller
     protected $baseUriSocket;
     public function __construct()
     {
-        $this->apiKey = "f2966f069e0c637f438a1e87b8b6a928";
+        // $this->apiKey = "f2966f069e0c637f438a1e87b8b6a928";
         // $this->baseUriSocket = "103.226.250.52";
-        $this->baseUriCall = "https://crm.pavietnam.vn";
+        // $this->baseUriCall = "https://crm.pavietnam.vn";
+        $this->baseUriCall = "https://voip.apps.vn";
+        $this->apiKey = "eec23ce1aac22627bb6d1b3115c8518a";
     }
+    // public function makeToCall($phone,$sip=0)
+    // {
+
+    //     $header=[];
+    //     $method = "POST";
+    //     $url = sprintf('%s/api/callNow.php',$this->baseUriCall);
+    //     $data_request = [
+    //         'api_key' => $this->apiKey,
+    //         'extension' => $sip,
+    //         'phone'   => $phone,
+    //     ];
+    
+    //     $ch = curl_init();
+    //     curl_setopt($ch, CURLOPT_URL, 'https://crm.pavietnam.vn/api/callNow.php');
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    //     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    //     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    //     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+    //     curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    //     curl_setopt($ch, CURLOPT_POST, true);
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data_request));
+    //     $result = curl_exec($ch);
+    //     u::logRequest($url,$method,$header,$data_request,$result,'log_request_outbound');
+
+    //     $res =json_decode($result);
+    //     if(data_get($res, 'code') == 1000){
+    //         $id = u::insertSimpleRow(array(
+    //             'phone' => $phone,
+    //             'sip_id' => $sip,
+    //             'created_at' => date('Y-m-d H:i:s')
+    //         ), 'pa_cdr_data');
+    //         return [
+    //             'status'=>1,
+    //             'call_id'=> $id,
+    //         ];
+    //     } else{
+    //         return [
+    //             'status'=>0,
+    //             'message'=> 'Thực hiện cuộc gọi thất bại, vui lòng thử lại'
+    //         ];
+    //     }
+        
+    // }
+    // public function getCDRReport($from_date)
+    // {
+    //     $data_request = [
+    //         'api_key' => $this->apiKey,
+    //         'to_date' => '',
+    //         'duration_max' => '',
+    //         'from_date' => $from_date,
+    //         'limit'   => '',
+    //         'destination' => '',
+    //         'duration_min' => '',
+    //         'source'  => '',
+    //         'status'  => '',
+    //         'page'    => '',
+    //         'from'    => '',
+    //     ];
+    
+    //     $ch = curl_init();
+    //     curl_setopt($ch, CURLOPT_URL, 'https://crm.pavietnam.vn/api/getCDRReport.php');
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    //     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    //     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    //     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+    //     curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    //     curl_setopt($ch, CURLOPT_POST, true);
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data_request));
+    //     $result = curl_exec($ch);
+    //     u::logRequest('https://crm.pavietnam.vn/api/getCDRReport.php','POST',[],$data_request,$result,'log_request_outbound');
+    //     $res =json_decode($result);
+    //     return $res;
+    // }
+
+    // Document VitalPBX https://documenter.getpostman.com/view/5481262/S17rvTgc?version=latest#feb244c3-1d4d-4e89-bbc1-0c54f6df2561
+
     public function makeToCall($phone,$sip=0)
     {
 
-        $header=[];
+        $header = array(
+            'app-key: '.$this->apiKey,
+            'tenant: 1',
+            'Content-Type: application/json',
+            'Cookie: HttpOnly; HttpOnly; HttpOnly'
+        );
         $method = "POST";
-        $url = sprintf('%s/api/callNow.php',$this->baseUriCall);
+        $url = sprintf('%s/api/v2/core/click_to_call',$this->baseUriCall);
         $data_request = [
-            'api_key' => $this->apiKey,
-            'extension' => $sip,
-            'phone'   => $phone,
+            'caller' => $sip,
+            'callee'   => $phone,
+            'cos_id'   => 1,
         ];
     
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://crm.pavietnam.vn/api/callNow.php');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data_request));
-        $result = curl_exec($ch);
-        u::logRequest($url,$method,$header,$data_request,$result,'log_request_outbound');
+        $res = curl::curl($url, $method,$header,$data_request);
+        u::logRequest($url,$method,$header,$data_request,$res,'log_request_outbound');
+        $res = json_decode($res);
 
-        $res =json_decode($result);
         if(data_get($res, 'code') == 1000){
             $id = u::insertSimpleRow(array(
                 'phone' => $phone,
@@ -68,88 +142,28 @@ class VoipController extends Controller
         }
         
     }
-    public function getCDRReport($from_date)
+    public function getCDRReport($sip_id)
     {
-        $data_request = [
-            'api_key' => $this->apiKey,
-            'to_date' => '',
-            'duration_max' => '',
-            'from_date' => $from_date,
-            'limit'   => '',
-            'destination' => '',
-            'duration_min' => '',
-            'source'  => '',
-            'status'  => '',
-            'page'    => '',
-            'from'    => '',
+        $header = array(
+            'app-key: '.$this->apiKey,
+            'tenant: 1'
+        );
+        $method = "GET";
+        $params = [
+            'extensions' => 411,
+            'calltype' => 3,
+            'start_date' => time() - 3600,
+            'end_date' => time(),
+            'limit'=>'10000',
+            'order_by'=>'calldate',
+            'order_type'=>'desc',
+            'disposition'=> 'ANSWER',
         ];
-    
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://crm.pavietnam.vn/api/getCDRReport.php');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data_request));
-        $result = curl_exec($ch);
-        u::logRequest('https://crm.pavietnam.vn/api/getCDRReport.php','POST',[],$data_request,$result,'log_request_outbound');
+        
+        $queryString = http_build_query($params);
+        $url = sprintf('%s/api/v2/cdr?%s',$this->baseUriCall,$queryString);
+        $result = curl::curl($url, $method, $header);
         $res =json_decode($result);
         return $res;
     }
-
-    public function test(){
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://cms.logiclab.vn/api/leads-create-checkin',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS =>'{
-            "student_name": "Nh\\u1eadt Huy",
-            "student_gender": "M",
-            "student_note": null,
-            "student_date_of_birth": "2020-09-25",
-            "gud_name": "Di\\u1ec7u Linh",
-            "gud_email": null,
-            "gud_mobile_1": "0399739128",
-            "gud_mobile_2": null,
-            "gud_gender": "F",
-            "gud_birthday": null,
-            "gud_job": null,
-            "source": 24,
-            "source_detail": null,
-            "address": null,
-            "province_id": null,
-            "district_id": null,
-            "branch_id": 1,
-            "checkin_at": "2024-11-09 00:00",
-            "type_product": "1",
-            "ec_hrm": "CM0114",
-            "creator_hrm": "C00001",
-            "sibling_id": 0
-        }',
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json'
-        ),
-        ));
-
-        $response = curl_exec($curl);
-
-        $err = curl_error($curl);
-        curl_close($curl);
-        if ($err) {
-            var_dump($response,$err);die();
-        } 
-        var_dump($response);die();
-
-
-    }
-    // Document VitalPBX https://documenter.getpostman.com/view/5481262/S17rvTgc?version=latest#feb244c3-1d4d-4e89-bbc1-0c54f6df2561
 }
