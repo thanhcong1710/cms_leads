c94986bebefb731bf99874c0f76e590a8f67fa08
thanhcong1710 <thanhcong1710@gmail.com>
2025-06-15 04:00:26 +0700

    update

--- resources/views/coreui/homepage.blade.php
+++ resources/views/coreui/homepage.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="locale" content="{{ App::getLocale() }}"/>
-    <link rel="icon" href="static/favicon.avif">
+    <link rel="icon" href="/static/favicon.avif">
     <title>LogicLab</title>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
