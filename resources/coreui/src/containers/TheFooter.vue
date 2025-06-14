5aa3548492c5af590815daf46457d6bb22489d86
thanhcong1710 <thanhcong1710@gmail.com>
2025-06-14 22:40:39 +0700

    update

--- resources/coreui/src/containers/TheFooter.vue
+++ resources/coreui/src/containers/TheFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <CFooter :fixed="false">
     <div>
-      <a href="https://cmsedu.vn/" target="_blank">CMS</a>
+      <a href="https://logiclab.edu.vn/" target="_blank">LogicLab</a>
       <span class="ml-1">&copy; {{new Date().getFullYear()}} creativeLabs.</span>
     </div>
     <div class="ml-auto">
       <span class="mr-1">Powered by</span>
-      <a href="https://cmsedu.vn/" target="_blank">CMS</a>
+      <a href="https://logiclab.edu.vn/" target="_blank">LogicLab</a>
     </div>
   </CFooter>
 </template>
--- resources/views/coreui/homepage.blade.php
+++ resources/views/coreui/homepage.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="locale" content="{{ App::getLocale() }}"/>
-    <link rel="icon" href="favicon.png">
-    <title>CMS Edu Việt Nam</title>
+    <link rel="icon" href="favicon.avif">
+    <title>LogicLab</title>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
     <script>
