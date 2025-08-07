4fa8cc3e0f045be5d94ae41ea3658a8e93f417cb
thanhcong1710 <thanhcong1710@gmail.com>
2025-08-07 21:58:05 +0700

    update

--- app/Http/Controllers/ParentsController.php
+++ app/Http/Controllers/ParentsController.php
@@ -551,7 +551,8 @@ class ParentsController extends Controller
                 p.last_care_date=(SELECT care_date FROM cms_customer_care WHERE parent_id=p.id AND creator_id=p.owner_id AND `status`=1 ORDER BY id DESC LIMIT 1) ,
                 p.care_date=(SELECT IF(care_date IS NULL, p.care_date,care_date) FROM cms_customer_care WHERE parent_id=p.id AND `status`=1 ORDER BY id DESC LIMIT 1)
             WHERE p.id=$parent_id ");
-        u::query("UPDATE cms_parents AS p LEFT JOIN users AS u ON u.id = p.owner_id SET p.tmp_branch_id = u.branch_id,p.is_lock = 1
+        u::query("UPDATE cms_parents AS p SET p.is_lock = 1 WHERE p.id=$parent_id ");
+        u::query("UPDATE cms_parents AS p LEFT JOIN users AS u ON u.id = p.owner_id SET p.tmp_branch_id = u.branch_id
             WHERE p.id=$parent_id  AND u.branch_id!=0 AND u.branch_id!= null");
         u::query("UPDATE cms_parents SET is_lock = 0 
             WHERE
