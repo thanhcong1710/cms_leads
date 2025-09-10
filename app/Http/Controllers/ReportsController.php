2387de0fdb85bc60f45a080b8dc8d560a91a7f51
thanhcong1710 <thanhcong1710@gmail.com>
2025-09-10 10:52:49 +0700

    update

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -238,14 +238,14 @@ class ReportsController extends Controller
         if($request->end_date){
             $cond .= " AND p.assign_date <= '".date('Y-m-d',strtotime($request->end_date))."'";
         }
-        $total = u::first("SELECT COUNT(*) AS total FROM (SELECT p.pre_owner_id, p.owner_id,p.assign_date  FROM cms_parent_assign AS p  
+        $total = u::first("SELECT COUNT(*) AS total FROM (SELECT p.pre_owner_id, p.owner_id,p.assign_date,p.creator_id  FROM cms_parent_assign AS p  
             WHERE $cond GROUP BY p.pre_owner_id,p.owner_id,p.assign_date ) AS t");
         $list = u::query("SELECT p.pre_owner_id, p.owner_id,p.assign_date, count(p.parent_id) AS total,
                 (SELECT CONCAT(name,' - ', hrm_id ) FROM users WHERE id = p.creator_id) AS creator_name,
                 (SELECT CONCAT(name,' - ', hrm_id ) FROM users WHERE id = p.pre_owner_id) AS pre_owner_name,
                 (SELECT CONCAT(name,' - ', hrm_id ) FROM users WHERE id = p.owner_id) AS owner_name
-            FROM cms_parent_assign AS p WHERE $cond GROUP BY p.pre_owner_id,p.owner_id,p.assign_date 
-            ORDER BY p.pre_owner_id DESC $limitation");
+            FROM cms_parent_assign AS p WHERE $cond GROUP BY p.pre_owner_id,p.owner_id,p.assign_date,p.creator_id 
+            ORDER BY p.assign_date DESC $limitation");
             
         $data = u::makingPagination($list, $total->total?? 0, $page, $limit);
         return response()->json($data);
