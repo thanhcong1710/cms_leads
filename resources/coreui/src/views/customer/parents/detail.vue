21f44bdcce0d2ea6c738300bdc4946fcf0ba6fea
thanhcong1710 <thanhcong1710@gmail.com>
2025-11-06 14:55:56 +0700

    update

--- resources/coreui/src/views/customer/parents/detail.vue
+++ resources/coreui/src/views/customer/parents/detail.vue
@@ -208,7 +208,7 @@
                               <p v-if="item.status>0">Cập nhật checkin: 
                                 <button :disabled="disabled_action" v-if="item.status>0" class="btn btn-sm btn-success" @click="showModalUpdateCheckin(item)"> <i class="fa fa-edit"></i></button>
                               </p>
-                              <p v-if="itm.crm_id">
+                              <p v-if="item.crm_id">
                                 <a :href="'https://crm.logiclab.vn/students/'+item.crm_id+ '?view=1'" target="_blank"> Xem thông tin</a>
                               </p>
                             </div>
