af4c51eff4203b02a5fc4576725415f0c951e05b
thanhcong1710 <thanhcong1710@gmail.com>
2025-11-12 18:00:38 +0700

    update

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -55,7 +55,7 @@ class StudentsController extends Controller
                 $contract_last =u::firstCRM("SELECT id, enrolment_last_date FROM contracts WHERe student_id= ".$student->crm_id." AND type>0 ORDER BY count_recharge DESC LIMIT 1");
                 $crm_student_info =u::firstCRM("SELECT status FROM students WHERe id= ".$student->crm_id);
                 $data[$k]->info_crm = $this->genStatus($contract_active, $contract_last);
-                $data[$k]->student_active = data_get($crm_student_info, 'staus') ? 1 : 0;
+                $data[$k]->student_active = data_get($crm_student_info, 'status') ? 1 : 0;
             }
         }
         return response()->json($data);
