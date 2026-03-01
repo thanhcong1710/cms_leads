6914399535aa0a92069c4de75844bf6da3905f9a
thanhcong1710 <thanhcong1710@gmail.com>
2026-03-01 19:52:00 +0700

    update

--- config/app.php
+++ config/app.php
@@ -230,5 +230,5 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
     ],
-    'group_user_ids' => env('GROUP_USER_IDS', '679,680,681,682,683,684,685,38,691,694')
+    'group_user_ids' => env('GROUP_USER_IDS', '')
 ];
